ff81c58da0d9c1bc0dafb8fbb2206737173233f8
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-02-11 11:03:31 +0100

    transfers hello systems to a plugin

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,18 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_startup_system(add_people)
-        .add_system(greet_people)
-        .add_system(hello_world)
+        .add_plugin(HelloPlugin)
         .run();
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(add_people)
+            .add_system(hello_world)
+            .add_system(greet_people);
+    }
 }
 
 fn hello_world() {
