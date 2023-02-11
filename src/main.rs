0982e2ce632bf923cfe3d4f71766b0f4e7ead8bc
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-02-11 10:52:39 +0100

    add a startup system: add_people

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
+        .add_startup_system(add_people)
         .add_system(hello_world)
         .run();
 }
@@ -15,3 +16,9 @@ struct Person;
 
 #[derive(Component)]
 struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commmands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commmands.spawn((Person, Name("Donald Duck".to_string())));
+    commmands.spawn((Person, Name("Mickey Mouse".to_string())));
+}
