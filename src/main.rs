f12c9ce2836624dca5bdbadec8aa19599b8844ad
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-02-11 15:35:01 +0100

    greeting every 2 seconds now, timer

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,8 @@ pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(add_people)
-            .add_system(hello_world)
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_startup_system(add_people)
             .add_system(greet_people);
     }
 }
@@ -27,14 +27,19 @@ struct Person;
 #[derive(Component)]
 struct Name(String);
 
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
 fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Elaina Proctor".to_string())));
     commands.spawn((Person, Name("Donald Duck".to_string())));
     commands.spawn((Person, Name("Mickey Mouse".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("Hello {}!", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("Hello {}!", name.0);
+        }
     }
 }
