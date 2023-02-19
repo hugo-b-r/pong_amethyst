6c653c6a3697edf2e66c63133c34e7ea1db502a7
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-02-19 16:42:13 +0100

    moving on the grid

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,12 @@ fn main() {
     App::new().add_startup_system(setup_camera)
                 .add_startup_system(spawn_snake)
                 .add_system(snake_movement)
+                .add_system_set_to_stage(
+                    CoreStage::PostUpdate,
+                    SystemSet::new()
+                        .with_system(position_translation)
+                        .with_system(size_scaling),
+                )
                 .add_plugins(DefaultPlugins)
                 .run();
         
@@ -71,10 +77,36 @@ struct Size{
 }
 
 impl Size {
-    pub fn Square(x: f32) -> Self {
+    pub fn square(x: f32) -> Self {
         Self {
             width: x,
             height: x,
         }
+    }
+}
+
+fn size_scaling(windows: Res<Windows>, mut q: Query<(&Size, &mut Transform)>) {
+    let window = windows.get_primary().unwrap();
+    for (sprite_size, mut transform) in q.iter_mut() {
+        transform.scale = Vec3::new(
+            sprite_size.width /ARENA_WIDTH as f32 * window.width() as f32,
+            sprite_size.height /ARENA_HEIGHT as f32 * window.height() as f32,
+            1.0,
+        );
+    }
+}
+
+fn position_translation(windows: Res<Windows>, mut q: Query<(&Position, &mut Transform)>) {
+    fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
+        let tile_size = bound_window / bound_game;
+        pos / bound_game * bound_window - (bound_window /2.) + (tile_size / 2.)
+    }
+    let window = windows.get_primary().unwrap();
+    for (pos, mut transform) in q.iter_mut() {
+        transform.translation = Vec3::new(
+            convert(pos.x as f32, window.width() as f32, ARENA_WIDTH as f32),
+            convert(pos.y as f32, window.height() as f32, ARENA_HEIGHT as f32),
+            0.0,
+        );
     }
 }
