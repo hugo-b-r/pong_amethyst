7d60f78159c9ef7d57815b06b0f5a1f3da3ff006
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-02-19 16:17:35 +0100

    moving snake

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use bevy::prelude::*;
 fn main() {
     App::new().add_startup_system(setup_camera)
                 .add_startup_system(spawn_snake)
+                .add_system(snake_movement)
                 .add_plugins(DefaultPlugins)
                 .run();
         
@@ -18,7 +19,7 @@ struct SnakeHead;
 const SNAKE_HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
 
 fn spawn_snake(mut commands: Commands) {
-    commands.spawn_bundle(SpriteBundle {
+    commands.spawn(SpriteBundle {
         sprite: Sprite {
             color: SNAKE_HEAD_COLOR,
             ..default()
@@ -30,4 +31,10 @@ fn spawn_snake(mut commands: Commands) {
         ..default()
     })
     .insert(SnakeHead);
+}
+
+fn snake_movement(mut head_positions: Query<(&SnakeHead, &mut Transform)>) {
+    for (_head, mut transform) in head_positions.iter_mut() {
+        transform.translation.y += 2.;
+    }
 }
