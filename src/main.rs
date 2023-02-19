a8bd65372322c574c4b7212c4b4c85f3908f2951
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-02-19 15:30:13 +0100

    new start to follow a snake tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,6 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugin(HelloPlugin)
-        .run();
-}
-
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-            .add_startup_system(add_people)
-            .add_system(greet_people);
-    }
-}
-
-fn hello_world() {
-    println!("hello world!");
-}
-
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
-
-#[derive(Resource)]
-struct GreetTimer(Timer);
-
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Donald Duck".to_string())));
-    commands.spawn((Person, Name("Mickey Mouse".to_string())));
-}
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in query.iter() {
-            println!("Hello {}!", name.0);
-        }
-    }
+    App::new().run();
+        
 }
