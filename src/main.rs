2f7c260a27a886a46620136bfdd39f4698b00a19
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-02-23 20:03:59 +0100

    snake segment color

--- src/main.rs
+++ src/main.rs
@@ -50,6 +50,7 @@ struct SnakeHead {
 
 const SNAKE_HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
 const FOOD_COLOR: Color = Color::rgb(1.0, 0.0, 1.0);
+const SNAKE_SEGMENT_COLOR : Color = Color::rgb(0.3, 0.3, 0.3);
 
 fn spawn_snake(mut commands: Commands) {
     commands.spawn(SpriteBundle {
